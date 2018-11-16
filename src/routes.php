d40baed7aff90f531fdb0555d01dea9a039038c5
Jop Bouckaert <jop.bouckaert@outlook.com>
2018-11-16 16:47:57 +0100

    add cvs route

--- src/routes.php
+++ src/routes.php
@@ -12,3 +12,12 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->post('/cvs', function (Request $request, Response $response, array $args) {
+    // Sample log message
+    $this->logger->info("POST /cvs");
+    
+
+    // Render index view
+    return $this->renderer->render($response, 'overview.phtml', $args);
+});
