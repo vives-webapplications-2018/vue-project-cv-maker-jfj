b5eb77b2904bc585855c83283bb5bb4eab7b4902
Jop Bouckaert <jop.bouckaert@outlook.com>
2018-11-22 21:58:06 +0100

    added collapsible overview, personal info education and expierence works

--- src/routes.php
+++ src/routes.php
@@ -46,18 +46,28 @@ $app->post('/cvs', function (Request $request, Response $response, array $args) 
 
     $address = new Address();
     $address->street = $request->getParam('street');
+    $args['streetname'] = $address->street;
     $address->nr = $request->getParam('nr');
+    $args['nr'] = $address->nr;
     $address->zip = $request->getParam('zip');
+    $args['zip'] = $address->zip;
     $address->city = $request->getParam('city');
+    $args['city'] = $address->city;
     $address->save();
 
     $user = new User();
     $user->firstname = $request->getParam('firstname');
+    $args['firstname'] = $user->firstname;
     $user->lastname = $request->getParam('lastname');
+    $args['lastname'] = $user->lastname;
     $user->email = $request->getParam('email');
+    $args['email'] = $user->email;
     $user->phonenumber = $request->getParam('phonenumber');
+    $args['phonenumber'] = $user->phonenumber;
     $user->birthdate = $request->getParam('birthdate');
+    $args['birthdate'] = $user->birthdate;
     $user->birthplace = $request->getParam('birthplace');
+    $args['birthplace'] = $user->birthplace;
     $user->githubusername = $request->getParam('githubUsername');
     $user->githubtoken = $request->getParam('githubToken');
     $user->addresses_id = $address->id;
@@ -65,21 +75,33 @@ $app->post('/cvs', function (Request $request, Response $response, array $args) 
 
     $education = new Education();
     $education->education = $request->getParam('education');
+    $args['education'] = $education->education;
     $education->place = $request->getParam('placeEdu');
+    $args['placeEdu'] = $education->place;
     $education->institute = $request->getParam('institute');
+    $args['institute'] = $education->institute;
     $education->fromEdu = $request->getParam('fromEdu');
+    $args['fromEdu'] = $education->fromEdu;
     $education->untilEdu = $request->getParam('untilEdu');
+    $args['untilEdu'] = $education->untilEdu;
     $education->information = $request->getParam('informationEdu');
+    $args['informationEdu'] = $education->informationEdu;
     $education->users_id = $user->id;
     $education->save();
 
     $experience = new Experience();
     $experience->functionExp = $request->getParam('functionExp');
+    $args['functionExp'] = $experience->functionExp;
     $experience->place = $request->getParam('placeExp');
+    $args['placeExp'] = $experience->place;
     $experience->employer = $request->getParam('employer');
+    $args['employer'] = $experience->employer;
     $experience->fromExp = $request->getParam('fromExp');
+    $args['fromExp'] = $experience->fromExp;
     $experience->untilExp = $request->getParam('untilExp');
+    $args['untilExp'] = $experience->untilExp;
     $experience->information = $request->getParam('informationExp');
+    $args['informationExp'] = $experience->informationExp;
     $experience->users_id = $user->id;
     $experience->save();
 
