b029d49bbdf93d973254a5297d3ea7999c9742cb
Jop Bouckaert <jop.bouckaert@outlook.com>
2018-11-22 20:19:27 +0100

    changed database to work with github api

--- Database/CVmaker.sql
+++ Database/CVmaker.sql
@@ -2,43 +2,6 @@ CREATE DATABASE CVmaker;
 
 USE CVmaker;
 
-CREATE TABLE Education (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  education varchar(32) NOT NULL,
-  place varchar(32) NULL,
-  institute varchar(32) NULL,
-  fromEdu TIMESTAMP NULL,
-  untilEdu TIMESTAMP NULL,
-  information text,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE Computerskills (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  skill varchar(32) NOT NULL,
-  level ENUM('verygood','good','normal') NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE Otherskills (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  skill varchar(32) NOT NULL,
-  level ENUM('verygood','good','normal') NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE Experiences (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  functionExp varchar(32) NOT NULL,
-  place varchar(32) NULL,
-  employer varchar(32) NOT NULL,
-  fromExp TIMESTAMP NULL,
-  untilExp TIMESTAMP NULL,
-  information text NULL,
-  PRIMARY KEY (id)
-);
-
-
 CREATE TABLE Addresses (
   id int(11) NOT NULL AUTO_INCREMENT,
   street varchar(40) NOT NULL,
@@ -60,14 +23,50 @@ CREATE TABLE Users (
   githubusername varchar(64) NULL,
   githubtoken varchar(64) NULL,
   addresses_id int(11) NULL,
-  educations_id int(11) NULL,
-  experiences_id int(11) NULL,
-  computerskills_id int(11) NULL,
-  otherskills_id int(11) NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (addresses_id) REFERENCES Addresses(id),
-  FOREIGN KEY (educations_id) REFERENCES Education(id),
-  FOREIGN KEY (experiences_id) REFERENCES Experiences(id),
-  FOREIGN KEY (computerskills_id) REFERENCES Computerskills(id),
-  FOREIGN KEY (otherskills_id) REFERENCES Otherskills(id)
+  FOREIGN KEY (addresses_id) REFERENCES Addresses(id)
+);
+
+CREATE TABLE Education (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  education varchar(32) NOT NULL,
+  place varchar(32) NULL,
+  institute varchar(32) NULL,
+  fromEdu TIMESTAMP NULL,
+  untilEdu TIMESTAMP NULL,
+  information text,
+  users_id int(11) NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (users_id) REFERENCES Users(id)
+);
+
+CREATE TABLE Computerskills (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  skill varchar(32) NOT NULL,
+  level varchar(32) NOT NULL,
+  users_id int(11) NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (users_id) REFERENCES Users(id)
+);
+
+CREATE TABLE Otherskills (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  skill varchar(32) NOT NULL,
+  level ENUM('verygood','good','normal') NOT NULL,
+  users_id int(11) NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (users_id) REFERENCES Users(id)
+);
+
+CREATE TABLE Experiences (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  functionExp varchar(32) NOT NULL,
+  place varchar(32) NULL,
+  employer varchar(32) NOT NULL,
+  fromExp TIMESTAMP NULL,
+  untilExp TIMESTAMP NULL,
+  information text NULL,
+  users_id int(11) NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (users_id) REFERENCES Users(id)
 );
