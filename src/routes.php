7d5f392b6cd1ebd366ea1c112cd494698e658637
Jop Bouckaert <jop.bouckaert@outlook.com>
2018-12-01 10:13:24 +0100

    changed url for presentation

--- src/routes.php
+++ src/routes.php
@@ -37,7 +37,7 @@ $app->get('/cv', function (Request $request, Response $response, array $args) {
 $app->get('/createcv', function (Request $request, Response $response, array $args) {
     $api_endpoint = "https://selectpdf.com/api2/convert/";
     $key = '84e33984-bb8a-46c1-89ca-f1f184de52c9';
-    $test_url = 'https://nl.lipsum.com/';
+    $test_url = 'https://jop-todo.herokuapp.com/';
     $local_file = 'cvs/test41.pdf';
  
     $parameters = array ('key' => $key, 'url' => $test_url);
