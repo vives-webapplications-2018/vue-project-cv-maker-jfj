b59efb88ffd6d08eda1fb126f4d00c45f71b9bab
Jop Bouckaert <jop.bouckaert@outlook.com>
2018-12-02 14:01:43 +0100

    add experience to overview

--- public/js/index.js
+++ public/js/index.js
@@ -16,7 +16,8 @@ const app = new Vue({
     countExp: 1,
     countComp: 1,
     countOth: 1,
-    educations: [{education:'',placeEdu:'',institute:'',fromEdu:'',untilEdu:'',informationEdu:''}]
+    educations: [{education:'',placeEdu:'',institute:'',fromEdu:'',untilEdu:'',informationEdu:''}],
+    experiences: [{function:'',placeExp:'',employer:'',fromExp:'',untilExp:'',informationExp:''}]
 
   },
   methods: {
@@ -25,6 +26,12 @@ const app = new Vue({
     },
     deleteEducation () {
       this.educations.pop({education:'',placeEdu:'',institute:'',fromEdu:'',untilEdu:'',informationEdu:''});
+    },
+    addExperience: function () {
+      this.experiences.push({function:'',placeExp:'',employer:'',fromExp:'',untilExp:'',informationExp:''});
+    },
+    deleteExperience () {
+      this.experiences.pop({function:'',placeExp:'',employer:'',fromExp:'',untilExp:'',informationExp:''});
     },
     incrementStep() {
       this.show++;
